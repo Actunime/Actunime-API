793ff65eb5c5a19f95563d47a7ce1bbdc0e41dcb
DevlM <dev.maudon@gmail.com>
2024-06-05 22:27:31 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -55,7 +55,7 @@ import Fastify_Cors from "@fastify/cors";
             console.log('Route', key, "chargé!");
         }
 
-        await fastify.listen({ port: 3000 });
+        await fastify.listen({ host: '0.0.0.0', port: 3000 });
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
