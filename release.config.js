242bfe9e2037e548240c773ebb6776390995d87c
DevlM <dev.maudon@gmail.com>
2024-06-06 01:58:29 +0200

    fix

--- release.config.js
+++ release.config.js
@@ -2,9 +2,6 @@ module.exports = {
     branches: ['production'],
     plugins: [
         '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/npm',
-        '@semantic-release/github'
     ],
     repositoryUrl: "https://github.com/Actunime/Actunime-API"
 };
