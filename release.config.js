f8d4b275c6ad0b923a2964415266cd9dc24c01a5
DevlM <dev.maudon@gmail.com>
2024-06-06 02:17:47 +0200

    fix: test

--- release.config.js
+++ release.config.js
@@ -2,7 +2,13 @@ module.exports = {
     branches: ['production'],
     plugins: [
         '@semantic-release/commit-analyzer',
-        '@semantic-release/git',
+        [
+            '@semantic-release/git',
+            {
+                "assets": ["package.json"],
+                "message": "chore(release): ${nextRelease.version} [skip ci]\\n\\n${nextRelease.notes}"
+            }
+        ]
     ],
     repositoryUrl: "https://github.com/Actunime/Actunime-API"
 };
