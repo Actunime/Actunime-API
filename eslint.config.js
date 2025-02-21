367b5a713326f68752d797d92a7c7d8bf56e55e4
DevlM <dev.maudon@gmail.com>
2025-02-21 01:49:25 +0100

    fix: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-const { config } = require("@actunime/eslint-config/build/base");
+const { config } = require("@actunime/eslint-config/base");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = config;
