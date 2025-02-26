8055387371c6d8a640925fb1ed27e9d070c047bf
DevlM <dev.maudon@gmail.com>
2025-02-26 20:53:32 +0100

    refactor(handler): handler qui gère la création d'un anime et ses relations en tant que modérateur

--- src/handlers/anime.handlers.ts
+++ src/handlers/anime.handlers.ts
@@ -5,9 +5,15 @@ import { APIResponse } from "../_utils/_response";
 import { AnimeCreateBody, AnimePaginationBody, ICreate_Anime_ZOD } from "@actunime/validations";
 import { RequestUtil } from "../_utils/_request";
 import { UserControllers } from "../controllers/user.controllers";
-import { IAnime, PatchTypeObj, TargetPathObj } from "@actunime/types";
+import { IAnime } from "@actunime/types";
 import { genPublicID } from "@actunime/utils";
 import { GroupeController } from "../controllers/groupe.controllers";
+import { ImageController } from "../controllers/image.controllers";
+import { MangaController } from "../controllers/manga.controller";
+import { CompanyController } from "../controllers/company.controller";
+import { PersonController } from "../controllers/person.controler";
+import { CharacterController } from "../controllers/character.controller";
+import { TrackController } from "../controllers/track.controller";
 
 const getAnimeById: RouteHandler = async (req) => {
     const { id } = z.object({ id: z.string() }).parse(req.params);
@@ -30,7 +36,7 @@ const filterAnime = async (req: FastifyRequest<{ Body: z.infer<typeof AnimePagin
     return new APIResponse({ success: true, data: animes });
 }
 
-const moderator_create_anime = async (req: FastifyRequest<{ Body: z.infer<typeof AnimeCreateBody> }>) => {
+const createAnime = async (req: FastifyRequest<{ Body: z.infer<typeof AnimeCreateBody> }>) => {
     const token = RequestUtil.getToken(req);
     const user = await UserControllers.getUserByToken(token)!;
 
@@ -45,12 +51,68 @@ const moderator_create_anime = async (req: FastifyRequest<{ Body: z.infer<typeof
     const controller = new AnimeController(req.session, user);
 
     if (groupe)
-        anime.groupe = await new GroupeController(req.session, user).create_relation(groupe, { refId, description, type: "CREATE" });
+        anime.groupe = await new GroupeController(req.session, user)
+            .create_relation(groupe, { refId, description, type: "MODERATOR_CREATE" });
 
     if (parent)
         anime.parent = await controller.create_relation(parent);
 
-    const res = await controller.create_patch(anime, { type: "MODERATOR_CREATE", description });
+    if (cover || banner) {
+        const imageController = new ImageController(req.session, user);
+
+        if (cover)
+            anime.cover = await imageController.create_relation(cover,
+                { refId, description, type: "MODERATOR_CREATE", targetPath: "Anime" }
+            );
+
+        if (banner)
+            anime.banner = await imageController.create_relation(banner,
+                { refId, description, type: "MODERATOR_CREATE", targetPath: "Anime" }
+            );
+    }
+
+    if (manga)
+        anime.manga = await new MangaController(req.session, user).create_relation(manga);
+
+    if (companys && companys.length > 0) {
+        const companyController = new CompanyController(req.session, user);
+        anime.companys = await Promise.all(companys.map((company) => {
+            return companyController.create_relation(company,
+                { refId, description, type: "MODERATOR_CREATE" }
+            );
+        }));
+    }
+
+    if (staffs && staffs.length > 0) {
+        const personController = new PersonController(req.session, user);
+        anime.staffs = await Promise.all(staffs.map((person) => {
+            return personController.create_relation(person,
+                { refId, description, type: "MODERATOR_CREATE" }
+            );
+        }));
+    }
+
+    if (characters && characters.length > 0) {
+        const characterController = new CharacterController(req.session, user);
+        anime.characters = await Promise.all(characters.map((character) => {
+            return characterController.create_relation(character,
+                { refId, description, type: "MODERATOR_CREATE" }
+            );
+        }));
+    }
+
+    if (tracks && tracks.length > 0) {
+        const trackController = new TrackController(req.session, user);
+        anime.tracks = await Promise.all(tracks.map((track) => {
+            return trackController.create_relation(track,
+                { refId, description, type: "MODERATOR_CREATE" }
+            );
+        }));
+    }
+
+    const res = await controller.create_patch(anime,
+        { type: "MODERATOR_CREATE", description, pathId: refId }
+    );
 
     return new APIResponse({ success: true, data: res });
 
@@ -58,5 +120,6 @@ const moderator_create_anime = async (req: FastifyRequest<{ Body: z.infer<typeof
 
 export const AnimeHandlers = {
     getAnimeById,
-    filterAnime
+    filterAnime,
+    createAnime
 };
