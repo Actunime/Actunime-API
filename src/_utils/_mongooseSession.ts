e89d75d12fc9ebea1430d0ca4043864dda6d1f7b
DevlM <dev.maudon@gmail.com>
2025-03-15 18:26:29 +0100

    fix: amélioration du système de session pour gérer les images

--- src/_utils/_mongooseSession.ts
+++ src/_utils/_mongooseSession.ts
@@ -3,6 +3,8 @@ import { MessageBuilder } from "discord-webhook-node";
 import { FastifyReply, FastifyRequest } from "fastify";
 import mongoose, { ClientSession } from "mongoose";
 import { APIDiscordWebhook } from "./_discordWebhook";
+import { ImageController } from "../controllers/image.controller";
+import { APIError } from "../_lib/Error";
 
 export const activesSessions = new Map<string, ClientSession>();
 
@@ -28,6 +30,7 @@ export const removeSessionHandler = async (req: FastifyRequest, res: FastifyRepl
         .setDescription(`Toutes les modifications précédentes ont été annulées !`);
 
     if (req.mongooseSession && req.mongooseSession.inTransaction()) {
+        const imageController = new ImageController(null);
         if (status >= 400) {
             await req.mongooseSession.abortTransaction();
             await req.mongooseSession.endSession();
@@ -38,14 +41,33 @@ export const removeSessionHandler = async (req: FastifyRequest, res: FastifyRepl
             // Pour éviter de sauvegarder pendant les tests
             const commit = true;
 
-            if (commit) await req.mongooseSession.commitTransaction();
+            if (commit) {
+                try {
+                    await Promise.all(ImageController.saveImages
+                        .filter((image) => image.session_id === req.mongooseSession?.id)
+                        .map(async (image) => {
+                            console.log("Création des fichiers images", image.data.id, image.data.path);
+                            await imageController.createImageFile(image.data);
+                        }));
+                    await Promise.all(ImageController.deleteImages
+                        .filter((image) => image.session_id === req.mongooseSession?.id)
+                        .map(async (image) => {
+                            console.log("Suppression des fichiers images", image.id, image.path);
+                            await imageController.deleteImageFile(image.id, image.path);
+                        }));
+                    await req.mongooseSession.commitTransaction();
+                } catch (error) {
+                    res.send(new APIError("Les fichiers des images n'ont pas pu étre crée/supprimé pour une raison inconnue", "SERVER_ERROR"));
+                    DevLog("SessionMongoose annulée", "warn");
+                }
+            }
             await req.mongooseSession.endSession();
             activesSessions.delete(req.id);
             DevLog("SessionMongoose validée", "debug");
 
-            if (commit)
+            if (commit) {
                 await APIDiscordWebhook.send(embedCommited);
-            else
+            } else
                 await APIDiscordWebhook.send(embedAborted);
         }
 
