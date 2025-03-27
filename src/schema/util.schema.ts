0fa1b0a92d6cd71546b5e1525de05f80d7665340
DevlM <dev.maudon@gmail.com>
2025-03-27 12:08:41 +0100

    fix: add patch to response validation

--- src/schema/util.schema.ts
+++ src/schema/util.schema.ts
@@ -2,14 +2,19 @@ import { z } from "zod"
 
 
 
-const ResponseBody = (data?: z.AnyZodObject | z.ZodString | z.ZodNull) => z.object({
+const ResponseBody = (
+  data?: z.AnyZodObject | z.ZodString | z.ZodNull,
+  patch?: z.AnyZodObject | z.ZodString | z.ZodNull
+) =>
+  z.object({
     success: z.boolean().default(true),
     message: z.string().optional(),
     code: z.string().optional(),
     error: z.union([z.string(), z.null()]).optional(),
     status: z.number().default(200),
-    data: data || z.any().optional()
-})
+    data: data || z.any().optional(),
+    patch: patch || z.any().optional()
+  });
 
 const UnauthorizedResponseBody = (data?: z.AnyZodObject | z.ZodString | z.ZodNull) => z.object({
     success: z.boolean().default(false),
